d1b090047f8af143089babd4f19f3f2133927090
go1985mez <jhoonconor1985@gmail.com>
2021-03-13 20:03:37 -0500

    ejercicio html arnold paredes

---
+++ homework HTML EJERCICIO ARNOLD PAREDES.js
@@ -0,0 +1,28 @@
+<html>
+
+    <head>
+        <title>Tarea html de Arnold paredes</title>
+<link rel="stylesheet" href="./styles.css">
+    </head>
+
+    <body>
+        <div class="divclass"><h1>Arnold Paredes</h1>
+             <H3>Henry</H3>
+             <h4> Tarea de HTML /CSS</h4>
+            </div>
+
+        <div class="divClass"><SPan id="spanId">Mi comida favorita: Ensalada de frutas</SPan>
+<a href="https://www.tripadvisor.co/Restaurant_Review-g294074-d15053069-Reviews-Cenizo_A_la_lena-Bogota.html ">Link a retaurante favorito </a>
+        </div>
+
+        <div id ="thirdDiv" >
+            <ul> 
+             <li><img src="https://t1.rg.ltmcdn.com/es/images/7/6/4/img_ensalada_de_frutas_para_diabeticos_57467_orig.jpg" > </li> 
+             <li><img src="https://www.demelito.com/wp-content/uploads/2018/03/ensalada-frutas-tradicional-mini-demelito.jpg" > </li>  
+            </ul>
+            
+        </div>
+        
+    </body>
+
+</html>
